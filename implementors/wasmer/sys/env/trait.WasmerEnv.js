3d844f53cf3d41c861a9e19c9baa3a094a8c2e4d
AurelienFT <32803821+AurelienFT@users.noreply.github.com>
2022-12-19 14:50:50 +0000

    Deploying to gh-pages from @ massalabs/as-ffi-bindings@d40a1586953d396508ef739f39f48e1a18e0b0cc 🚀

--- implementors/wasmer/sys/env/trait.WasmerEnv.js
+++
@@ -1,3 +0,0 @@
-(function() {var implementors = {
-"as_ffi_bindings":[["impl WasmerEnv for <a class=\"struct\" href=\"as_ffi_bindings/struct.Env.html\" title=\"struct as_ffi_bindings::Env\">Env</a>"]]
-};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
