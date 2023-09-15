6b43aacd461b6fb5b7fb9efebd9096ff48bf9bb2
AurelienFT <32803821+AurelienFT@users.noreply.github.com>
2023-09-15 10:08:33 +0000

    Deploying to gh-pages from @ massalabs/as-ffi-bindings@d5e880a61ec8c9d8162ced88efbfbe495d693c62 🚀

--- src-files.js
+++ src-files.js
@@ -1,4 +1,4 @@
-var sourcesIndex = JSON.parse('{\
+var srcIndex = JSON.parse('{\
 "as_ffi_bindings":["",[],["any_ptr.rs","buffer_ptr.rs","env.rs","lib.rs","string_ptr.rs","tools.rs"]]\
 }');
-createSourceSidebar();
+createSrcSidebar();
